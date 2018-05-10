eed2a8142e4d4afbf576825f82e3df1d410d26b8
JACKSON\jackson <765596212@qq.com>
2018-05-10 16:01:28 +0800

    change projects

---
+++ base/src/main/java/com/lxtx/guava/GuavaTest.java
@@ -0,0 +1,27 @@
+package com.lxtx.guava;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Multiset;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author jackson
+ */
+public class GuavaTest {
+    public static void main(String args[]){
+        GuavaTest guavaTester = new GuavaTest();
+        Integer invalidInput = null;
+        Optional<Integer> a =  Optional.of(invalidInput);
+        Optional<Integer> b =  Optional.of(new Integer(10));
+        System.out.println(guavaTester.sum(a,b));
+
+//        Multiset.Entry
+    }
+
+    public Integer sum(Optional<Integer> a, Optional<Integer> b){
+        return a.get() + b.get();
+    }
+}
