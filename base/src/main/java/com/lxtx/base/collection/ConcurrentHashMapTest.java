f383ee515aba82d4a9348a250036802320e442ed
JACKSON\jackson <765596212@qq.com>
2018-06-01 14:42:26 +0800

    ConcurrentHashMap

---
+++ base/src/main/java/com/lxtx/base/collection/ConcurrentHashMapTest.java
@@ -0,0 +1,56 @@
+package com.lxtx.base.collection;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ConcurrentHashMap  test
+ * @author 孙洪波
+ * @version 1.0  2018/6/1
+ */
+public class ConcurrentHashMapTest {
+
+    static Map<Long, String> conMap = new ConcurrentHashMap<Long, String>();
+    public static void main(String[] args) throws InterruptedException {
+        for (long i = 0; i < 5; i++) {
+            conMap.put(i, i + "");
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                conMap.put(100l,"100121121221");
+                System.out.println("ADD:" + 100);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (Iterator<Map.Entry<Long, String>> iterator = conMap.entrySet().iterator(); iterator.hasNext();) {
+                    Map.Entry<Long, String> entry = iterator.next();
+                    System.out.println(entry.getKey() + " - " + entry.getValue());
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+        thread2.start();
+
+        Thread.sleep(3000);
+        for (Map.Entry<Long, String> entry : conMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
+}
