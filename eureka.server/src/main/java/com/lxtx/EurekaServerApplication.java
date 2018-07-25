ac26a512e518bc3e592286de5f349c2b8b2fa572
lxtx021 <sunhongbo007@gmail.com>
2018-07-25 15:58:55 +0800

    eureka server add

---
+++ eureka.server/src/main/java/com/lxtx/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.lxtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka 註冊中心 server 
+ * @author jackson
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
