3ba7aeca6b394406008bacf5cb6eb677f5def82c
lxtx021 <sunhongbo007@gmail.com>
2018-08-23 14:05:50 +0800

    mq

--- rabbitmq.producer/src/main/java/com/lxtx/rabbitmq/RabbitConfig.java
+++ rabbitmq.producer/src/main/java/com/lxtx/rabbitmq/RabbitConfig.java
@@ -7,7 +7,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+/*
+    @Bean
+    public Queue helloQueue() {
+        return new Queue("hello");
+    }*/
 
+    @Bean
+    public Queue neoQueue() {
+        return new Queue("neo");
+    }
 
     @Bean
     public Queue objectQueue() {
