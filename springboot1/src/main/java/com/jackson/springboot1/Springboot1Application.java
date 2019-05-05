cfca09e22d0994313be9f5f992ce408f0d259ef4
sun <765596212@qq.com>
2019-05-05 16:42:16 +0800

    submit springboot 2.X demo

---
+++ springboot1/src/main/java/com/jackson/springboot1/Springboot1Application.java
@@ -0,0 +1,47 @@
+package com.jackson.springboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author sun
+ */
+@SpringBootApplication
+@Configuration
+@EnableSwagger2
+public class Springboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot1Application.class, args);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jackson.springboot1.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使⽤Swagger2构建RESTful APIs")
+                .description("Spring Boot中使⽤Swagger2构建RESTful APIs")
+                        .termsOfServiceUrl("localhost:9000/")
+                        .contact("jackson")
+                        .version("1.0")
+                        .build();
+    }
+
+}
